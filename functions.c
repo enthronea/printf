ceea4c4b7b40c101a40a0da6e33f2aea67d5c252
precious <enthronepresh@yahoo.com>
2023-01-21 11:58:29 -0800

    taskf

--- functions.c
+++ functions.c
@@ -22,7 +22,7 @@ int flags, int width, int precision, int size)
 {
 	char c = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(c, buffer, flags, width, precision, size));;
 }
 /************************* PRINT A STRING *************************/
 /**
@@ -199,7 +199,7 @@ int flags, int width, int precision, int size)
 	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
 		sum += a[i];
-		if (sum || i == 3)
+		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
 
